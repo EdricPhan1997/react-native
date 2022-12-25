f8e431ad3fb3f8f0d3e15bbd3797207b5489d90f
Edric <devpch.wwp@gmail.com>
2022-12-25 16:45:18 +0700

    update code learn button text input

--- screens/Learn/LearnTextImputAndKeyboard.js
+++ screens/Learn/LearnTextImputAndKeyboard.js
@@ -1,12 +1,146 @@
-import React from "react";
-import { View, Text, Image, TextInput } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Pressable,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const LearnTextImputAndKeyboard = () => {
+  const [name, setName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const hanldeClick = () => {
+    //
+    setSubmitted(!submitted);
+  };
+
   return (
-    <View>
-      <Text>Hieu</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.view1}>
+        <Text style={styles.text}>Please write your name !!!</Text>
+        <TextInput
+          // multiline
+          style={styles.input}
+          placeholder="e.g name ???"
+          onChangeText={(value) => setName(value)}
+          // keyboardType= 'ascii-capable'
+          // maxLength={10}
+          // editable={false}
+          // secureTextEntry
+        />
+        {/* <Button
+          title={submitted ? "Clear" : "submit"}
+          onPress={hanldeClick}
+          // disabled={submitted}
+          color="#00f"
+        /> */}
+
+        {/* <TouchableOpacity style={styles.button} onPress={hanldeClick} activeOpacity={0.2}>
+          <Text>{submitted ? "Clear" : "submit"}</Text>
+        </TouchableOpacity> */}
+        {/* <TouchableHighlight
+          style={styles.button}
+          onPress={hanldeClick}
+          underlayColor="#dddd"
+        >
+          <Text>{submitted ? "Clear" : "submit"}</Text>
+        </TouchableHighlight> */}
+
+        {/* <TouchableWithoutFeedback onPress={hanldeClick}>
+          <View style={styles.button}>
+            <Text>{submitted ? "Clear" : "submit"}</Text>
+          </View>
+        </TouchableWithoutFeedback> */}
+        <Pressable
+          // click lau lau
+          // onLongPress={hanldeClick}
+          // delayLongPress={2000}
+          style={({ pressed }) => [
+            { backgroundColor: pressed ? "#dddd" : "#00ff00" },
+            styles.button,
+          ]}
+          onPress={hanldeClick}
+          // Click xung quanh voi pham vi tuy chinh
+          hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
+          android_ripple={{ color: "#00f" }}
+        >
+          <Text>{submitted ? "Clear" : "submit"}</Text>
+        </Pressable>
+        {submitted && <Text style={styles.text}>your is Name : {name}</Text>}
+      </View>
+    </SafeAreaView>
   );
 };
 
+/*
+  Todo: mutiline : dung de nhap nhieu dong khi enter tu dong xuong dong chu khong submit
+   * maxLength : nhap toi da bao nhieu ky tu
+  * editable= {false} : disable khong cho nguoi dung nhap du lieu
+  * secureTextEntry === passworld
+*/
+
+/*
+  Todo: Button 
+    * onPress == onClick
+
+  ! Do Button it tinh nang nen ta dung TouchableOpacity de thay the
+
+
+  Todo: TouchableOpacity
+  * activeOpacity: xac dinh do opacity khi click
+
+  Todo TouchableHighlight => giong  nhu TouchableOpacity, nhung khong co do opacity mac dinh ma phai tuy chinh qua params underlayColor = "color"
+
+  Todo TouchableWithoutFeedback : khong bao ra cam giac khi click
+*/
+
 export default LearnTextImputAndKeyboard;
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    backgroundColor: "#0000ff",
+  },
+  view1: {
+    // width: "100%",
+    height: 100,
+    margin: 10,
+    // backgroundColor: "red",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 24,
+    color: "#000",
+    marginTop: 10,
+  },
+  full: {
+    width: "100%",
+    height: "100%",
+  },
+  input: {
+    borderWidth: 1,
+    width: 200,
+    height: 40,
+    borderColor: "red",
+    borderRadius: 5,
+    margin: 10,
+    textAlign: "center",
+  },
+  button: {
+    // backgroundColor: "#00ff00",
+    padding: 8,
+  },
+});
